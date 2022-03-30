9f35489eafdf0536e68088b1b614d35dc5ff5857
Abdullah-yasir <56272322+Abdullah-yasir@users.noreply.github.com>
2022-03-30 13:58:08 +0500

    server works with static data

--- src/server.js
+++ src/server.js
@@ -4,16 +4,105 @@ const { graphqlHTTP } = require('express-graphql');
 
 const schema = buildSchema(`
    type Query {
-      hello: String
-      bye: String
-      greet(name: String): String
+      products: [Product]
+      orders: [Order]
+      login(name: String): String
+   }
+
+   type Product {
+      id: ID!
+      name: String!
+      price: Float!
+      reviews: [Review]
+      description: String!
+   }
+
+   type Review {
+      id: ID!
+      text: String!
+      rating: Int!
+   }
+
+   type Order {
+      id: ID!
+      date: String!
+      items: [OrderItem]
+      subtotal: Float!
+   }
+
+   type OrderItem {
+      id: ID!
+      product: Product!
+      quantity: Int!
    }
 `);
 
 const root = {
-   hello: () => 'Hello world!',
-   bye: () => 'Goodbye world!',
-   greet: (args) => `Hello ${args.name}!`
+   products: [
+      {
+         id: '1',
+         name: 'Product 1',
+         description: 'This is a product',
+         price: 10,
+         reviews: [
+            {
+               id: '1',
+               text: 'This is a great product!',
+               rating: 5
+            },
+            {
+               id: '2',
+               text: 'This is a bad product!',
+               rating: 1
+            }
+         ]
+      },
+      {
+         id: '2',
+         name: 'Product 2',
+         description: 'This is a product',
+         price: 20,
+         reviews: [
+            {
+               id: '3',
+               text: 'This is a great product!',
+               rating: 5
+            },
+            {
+               id: '4',
+               text: 'This is a bad product!',
+               rating: 1
+            }
+         ]
+      }
+   ],
+   orders: [
+      {
+         id: '1',
+         date: '2018-01-01',
+         subtotal: 10,
+         items: [
+            {
+               id: '1',
+               product: {
+                  id: '1',
+                  name: 'Product 1',
+                  description: 'This is a product',
+                  price: 10,
+                  reviews: [
+                     {
+                        id: '1',
+                        text: 'This is a great product!',
+                        rating: 5
+                     },
+                  ]
+               },
+               quantity: 2
+            },
+         ]
+      },
+   ],
+   login: (args) => `Hello ${args.name}!`
 };
 
 const app = express();
